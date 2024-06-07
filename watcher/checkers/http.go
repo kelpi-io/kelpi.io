8a6b6db4f651b3c1f0d5e3699928fc2d86872b59
Valeriy Ishutin <ishutin_va@outlook.com>
2024-06-07 22:01:06 +0300

    Add uUser-Agent header

--- watcher/checkers/http.go
+++ watcher/checkers/http.go
@@ -65,6 +65,8 @@ func HttpCheck(config WatcherConfig, memberName string) interface{} {
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Host = monitorParam.Host
 
+	req.Header.Add("User-Agent", "gslb-operator/1.0.0")
+
 	for k, v := range monitorParam.Headers {
 		req.Header.Add(k, v)
 	}
